c137796f334711bf2ae082b9394e22ef3c2c0326
kfcman21 <kfcman21@naver.com>
2026-06-16 21:53:24 +0900

    fix: A4 table column widths - criterion 10%, improvement 22%, analysis 15%

--- app.js
+++ app.js
@@ -3123,13 +3123,13 @@ function renderA4Preview() {
         <tr>
           <th style="width: 10%">대분류 (요소)</th>
           <th style="width: 12%">중분류 (실증항목)</th>
-          <th style="width: 14%">점검 기준 (교사 커스텀 재수정 ✍️)</th>
-          <th style="width: 6%">구분</th>
-          <th style="width: 20%">실제 교실 분석내용 및 현상</th>
-          <th style="width: 6%">심각성</th>
-          <th style="width: 18%">개선 요청사항</th>
-          <th style="width: 7%">상황설명 사진</th>
-          <th style="width: 7%">유튜브 동영상</th>
+          <th style="width: 10%">점검 기준 (교사 커스텀 재수정 ✍️)</th>
+          <th style="width: 5%">구분</th>
+          <th style="width: 15%">실제 교실 분석내용 및 현상</th>
+          <th style="width: 5%">심각성</th>
+          <th style="width: 22%">개선 요청사항</th>
+          <th style="width: 11%">상황설명 사진</th>
+          <th style="width: 10%">유튜브 동영상</th>
         </tr>
       </thead>
       <tbody></tbody>
